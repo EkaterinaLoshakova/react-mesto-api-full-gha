48cdad3db81aa9d04a94865557fec9d39cd66cdb
Ekaterina Loshakova <katyushka8905@yandex.ru>
2023-09-04 20:05:10 +0300

    com13

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,16 @@
-require("dotenv").config();
-const mongoose = require("mongoose");
-const express = require("express");
-const bodyParser = require("body-parser");
-const { errors } = require("celebrate");
-const helmet = require("helmet");
-const rateLimit = require("express-rate-limit");
-const cors = require("cors");
-const { requestLogger, errorLogger } = require("./middlewares/logger");
+require('dotenv').config();
+const mongoose = require('mongoose');
+const express = require('express');
+const bodyParser = require('body-parser');
+const { errors } = require('celebrate');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
+const cors = require('cors');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 // const { notFoundStatus } = require('./utils/constants');
-const auth = require("./middlewares/auth");
-const NotFoundError = require("./errors/NotFoundError");
-const errorHandler = require("./middlewares/error-handler");
+const auth = require('./middlewares/auth');
+const NotFoundError = require('./errors/NotFoundError');
+const errorHandler = require('./middlewares/error-handler');
 
 const app = express();
 
@@ -24,26 +24,26 @@ app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect("mongodb://127.0.0.1:27017/mesto");
+mongoose.connect('mongodb://127.0.0.1:27017/mesto');
 
 const { PORT = 3000 } = process.env;
 
 app.use(requestLogger);
 app.use(limiter);
-app.get("/crash-test", () => {
+app.get('/crash-test', () => {
   setTimeout(() => {
-    throw new Error("Сервер сейчас упадёт");
+    throw new Error('Сервер сейчас упадёт');
   }, 0);
 });
-app.use("/signup", require("./routes/signup"));
-app.use("/signin", require("./routes/signin"));
+app.use('/signup', require('./routes/signup'));
+app.use('/signin', require('./routes/signin'));
 
 app.use(auth);
-app.use("/users", require("./routes/users"));
-app.use("/cards", require("./routes/cards"));
+app.use('/users', require('./routes/users'));
+app.use('/cards', require('./routes/cards'));
 
-app.use("*", (req, res, next) => {
-  next(new NotFoundError({ message: "Такой старницы не существует" }));
+app.use('*', (req, res, next) => {
+  next(new NotFoundError({ message: 'Такой старницы не существует' }));
 });
 
 app.use(errorLogger);
